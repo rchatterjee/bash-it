028840eec2549e58af73d30d2cb1e835b9b99e0b
Rahul Chatterjee <rahul@cs.cornell.edu>
2016-04-05 11:08:10 -0400

    My modifications

--- aliases/available/emacs.aliases.bash
+++ aliases/available/emacs.aliases.bash
@@ -2,15 +2,12 @@ cite 'about-alias'
 about-alias 'emacs editor'
 
 case $OSTYPE in
-  linux*)
+  darwin*)
     alias em='emacs'
     alias en='emacs -nw'
     alias e='emacsclient -n'
     alias et='emacsclient -t'
     alias ed='emacs --daemon'
     alias E='SUDO_EDITOR=emacsclient sudo -e'
-    ;;
-  darwin*)
-    alias em='open -a emacs'
     ;;
 esac
